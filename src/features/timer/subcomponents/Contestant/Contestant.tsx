8552f9232f9573fcdcbc2113a4bc7aa2c15b3a76
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-06 19:29:37 +0200

    feat: add modifier to detect the second contestant

--- src/features/timer/subcomponents/Contestant/Contestant.tsx
+++ src/features/timer/subcomponents/Contestant/Contestant.tsx
@@ -4,18 +4,25 @@ import { ContestantType } from '../../timerSlice';
 
 import styles from './Contestant.module.scss';
 
-type ContestantProps = ContestantType;
+type ContestantModifier = {
+  isSecond: boolean
+};
+type ContestantProps = ContestantType & ContestantModifier;
 
-function Contestant({ color, lastName, rank }: ContestantProps): JSX.Element {
+function Contestant({
+  color, isSecond, lastName, rank,
+}: ContestantProps): JSX.Element {
   const [name, setName] = useState(lastName);
   const [ranking, setRanking] = useState(rank);
   const [actualColor, setActualColor] = useState(color);
+  const modifierClass = isSecond ? ` ${styles.colorSignSecond}` : '';
 
   return (
     <div className={styles.contestant}>
-      <div className={styles.textContainer}>
+      <div className={styles.container}>
         <span className={styles.rank}>{ranking}</span>
         <span className={styles.name}>{name}</span>
+        <span className={`${styles.colorSign}${modifierClass}`} />
       </div>
       <div className={styles.color}>
         <span className={styles.colorText}>{actualColor}</span>
