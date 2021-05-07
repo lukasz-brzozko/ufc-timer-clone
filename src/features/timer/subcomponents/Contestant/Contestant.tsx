7610a696d7b1e8cb99eb72130e711da17c1eb49a
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-07 17:35:42 +0200

    feat: add necessary refs for an animation

--- src/features/timer/subcomponents/Contestant/Contestant.tsx
+++ src/features/timer/subcomponents/Contestant/Contestant.tsx
@@ -7,10 +7,27 @@ import styles from './Contestant.module.scss';
 type ContestantModifier = {
   isSecond: boolean
 };
-type ContestantProps = ContestantType & ContestantModifier;
+
+type ContestantRefs = {
+  refContestant: React.RefObject<HTMLDivElement>
+  refColor: React.RefObject<HTMLDivElement>
+  refColorSign: React.RefObject<HTMLDivElement>
+  refText: React.RefObject<HTMLDivElement>
+  refTextBlock: React.RefObject<HTMLDivElement>
+};
+
+type ContestantProps = ContestantModifier & ContestantRefs & ContestantType;
 
 function Contestant({
-  color, isSecond, lastName, rank,
+  color,
+  isSecond,
+  lastName,
+  rank,
+  refContestant,
+  refColor,
+  refColorSign,
+  refText,
+  refTextBlock,
 }: ContestantProps): JSX.Element {
   const [name, setName] = useState(lastName);
   const [ranking, setRanking] = useState(rank);
@@ -18,13 +35,15 @@ function Contestant({
   const modifierClass = isSecond ? ` ${styles.colorSignSecond}` : '';
 
   return (
-    <div className={styles.contestant}>
-      <div className={styles.container}>
-        <span className={styles.rank}>{ranking}</span>
-        <span className={styles.name}>{name}</span>
-        <span className={`${styles.colorSign}${modifierClass}`} />
+    <div className={styles.contestant} ref={refContestant}>
+      <div className={styles.container} ref={refTextBlock}>
+        <div className={styles.textWrapper} ref={refText}>
+          <span className={styles.rank}>{ranking}</span>
+          <span className={styles.name}>{name}</span>
+        </div>
+        <span className={`${styles.colorSign}${modifierClass}`} ref={refColorSign} />
       </div>
-      <div className={styles.color}>
+      <div className={styles.color} ref={refColor}>
         <span className={styles.colorText}>{actualColor}</span>
       </div>
     </div>
