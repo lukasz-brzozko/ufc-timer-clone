9393ab7674264154e3ae1ac4e098c10eaf96265f
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-13 17:18:55 +0200

    feat: add Bouts constant

---
+++ src/constants/Bouts.ts
@@ -0,0 +1,13 @@
+enum BOUTS {
+  STRAWWEIGHT = 'Strawweight Bout',
+  FLYWEIGHT = 'Flyweight Bout',
+  BANTAMWEIGHT = 'Bantamweight Bout',
+  FEATHERWEIGHT = 'Featherweight Bout',
+  LIGHTWEIGHT = 'Lightweight Bout',
+  WELTERWEIGHT = 'Welterweight Bout',
+  MIDDLEWEIGHT = 'Middleweight Bout',
+  LIGHT_HEAVYWEIGHT = 'Light Heavyweight Bout',
+  HEAVYWEIGHT = 'Heavyweight Bout',
+}
+
+export default BOUTS;
