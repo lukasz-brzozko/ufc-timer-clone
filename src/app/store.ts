fea599a609b4c9ff7fe4896cd3376d07598161d9
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-13 17:18:39 +0200

    fix: remove global timeline from the redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import infoReducer from '../features/info/infoSlice';
-import timelineReducer from '../features/timeline/timelineSlice';
 import timerReducer from '../features/timer/timerSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     info: infoReducer,
-    timeline: timelineReducer,
     timer: timerReducer,
   },
 });
