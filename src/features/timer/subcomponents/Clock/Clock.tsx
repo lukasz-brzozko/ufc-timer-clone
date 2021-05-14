9d5e631f91d734bb1b924bdd7688f30e45efa805
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-14 20:55:48 +0200

    feat: add clock entrance animation

--- src/features/timer/subcomponents/Clock/Clock.tsx
+++ src/features/timer/subcomponents/Clock/Clock.tsx
@@ -1,34 +1,65 @@
+import {
+  RefObject, useCallback, useEffect, useRef,
+} from 'react';
+import gsap from 'gsap';
 import Countdown, { zeroPad } from 'react-countdown';
 
 import Info from '../../../info/Info';
+import Logo from '../Logo';
 import RoundCounter from '../RoundCounter';
-import { ReactComponent as UFCLogo } from '../../../../assets/ufc-logo.svg';
 
 import styles from './Clock.module.scss';
 
 interface RendererProps {
-  hours: number;
   minutes: number;
   seconds: number;
 }
 
 function Clock(): JSX.Element {
-  const renderer = ({ minutes, seconds }: RendererProps) => (
-    <span className={styles.time}>
-      <span className={styles.minutes}>{zeroPad(minutes, 1)}</span>
-      <span className={styles.separator}>:</span>
-      <span className={styles.seconds}>{zeroPad(seconds, 2)}</span>
-    </span>
+  const refClock = useRef(null);
+
+  const showClock = useCallback(
+    () => {
+      const tl = gsap.timeline({
+        defaults: {
+          duration: '0.4',
+          ease: 'power1.inOut',
+        },
+        id: 'showClock',
+      });
+      tl
+        .set(refClock.current, { height: '200%' })
+        .fromTo(refClock.current, { rotateX: '90deg', y: '200%' }, { ease: 'power2.in', rotateX: '0deg', y: '25%' })
+        .to(refClock.current, { height: '100%', y: '0%' }, '>');
+    },
+    [refClock],
   );
 
+  const renderer = useCallback(
+    (props: RendererProps) => (
+      <>
+        <span className={styles.time}>
+          <span className={styles.minutes}>{zeroPad(props.minutes, 1)}</span>
+          <span className={styles.separator}>:</span>
+          <span className={styles.seconds}>{zeroPad(props.seconds, 2)}</span>
+        </span>
+      </>
+    ),
+    [],
+  );
+
+  useEffect(() => {
+    showClock();
+  }, [refClock]);
+
   return (
-    <div className={styles.clock}>
+    <div className={styles.clock} ref={refClock}>
       <Info />
-      <UFCLogo className={styles.ufcLogo} />
-      <Countdown
+      <Logo />
+      {/* <Countdown
         date={Date.now() + 5 * 60 * 1000}
         renderer={renderer}
-      />
+      /> */}
       <RoundCounter />
     </div>
   );
