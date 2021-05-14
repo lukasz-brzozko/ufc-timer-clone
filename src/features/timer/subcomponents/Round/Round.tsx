d1379a703806d16c0720dbe43b6724fb60b02dc3
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-14 18:22:17 +0200

    feat: add Round component

--- src/features/timer/subcomponents/Round/Round.tsx
+++ src/features/timer/subcomponents/Round/Round.tsx
@@ -2,14 +2,17 @@ import styles from './Round.module.scss';
 
 function Round() {
   return (
-    <>
+    <div className={styles.roundContainer}>
+      <div className={styles.roundWrapper}>
+        <span className={`${styles.round} ${styles.roundComplete}`} />
+      </div>
       <div className={styles.roundWrapper}>
         <span className={`${styles.round} ${styles.roundActive}`} />
       </div>
       <div className={styles.roundWrapper}>
         <span className={`${styles.round}`} />
       </div>
-    </>
+    </div>
   );
 }
 
