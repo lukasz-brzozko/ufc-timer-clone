a7e27bdbc9c16391e0ddfccdc459836cb316044d
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-14 20:54:59 +0200

    feat: add round number entrance animation

--- src/features/timer/subcomponents/RoundCounter/RoundCounter.tsx
+++ src/features/timer/subcomponents/RoundCounter/RoundCounter.tsx
@@ -1,12 +1,40 @@
+import { useCallback, useEffect, useRef } from 'react';
+import gsap from 'gsap';
 import Round from '../Round';
 
 import styles from './RoundCounter.module.scss';
 
 function RoundCounter(): JSX.Element {
+  const refRoundNumber = useRef<HTMLSpanElement>(null);
+
+  const showCurrentRound = useCallback(
+    () => {
+      const tl = gsap.timeline({
+        defaults: {
+          duration: '0.15',
+        },
+        id: 'showCurrentRound',
+      });
+
+      tl
+        .to(refRoundNumber.current, { duration: '0.3', ease: 'power1.inOut', y: '0%' })
+        .to(refRoundNumber.current, { autoAlpha: 0 }, '+=0.3')
+        .to(refRoundNumber.current, { autoAlpha: 1 })
+        .to(refRoundNumber.current, { autoAlpha: 0 })
+        .to(refRoundNumber.current, { autoAlpha: 1 })
+        .to(refRoundNumber.current, { duration: '0.3', ease: 'power2.inOut', y: '100%' }, '+=0.4');
+    },
+    [refRoundNumber],
+  );
+
+  useEffect(() => {
+    showCurrentRound();
+  }, []);
+
   return (
     <>
       <div className={styles.roundCounterWrapper}>
-        <span className={styles.roundNumber}>Round 1</span>
+        <span className={styles.roundNumber} ref={refRoundNumber}>Round 1</span>
       </div>
       <Round />
     </>
