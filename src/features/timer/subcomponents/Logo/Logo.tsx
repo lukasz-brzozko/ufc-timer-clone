30b876013b0fea6002044f5558e284381c798ec3
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-17 19:46:17 +0200

    feat: add additional animations with background

--- src/features/timer/subcomponents/Logo/Logo.tsx
+++ src/features/timer/subcomponents/Logo/Logo.tsx
@@ -5,7 +5,8 @@ import { ReactComponent as UFCLogo } from '../../../../assets/ufc-logo.svg';
 import styles from './Logo.module.scss';
 
 function Logo(): JSX.Element {
-  const refLogo = useRef <SVGSVGElement>(null);
+  const refLogo = useRef<SVGSVGElement>(null);
+  const refLogokWrapper = useRef<HTMLDivElement>(null);
 
   const showLogo = useCallback(
     () => {
@@ -23,12 +24,47 @@ function Logo(): JSX.Element {
     [refLogo],
   );
 
+  const hideLogoWrapperBackground = useCallback(
+    () => {
+      const tl = gsap.timeline({
+        defaults: {
+          duration: '0.1',
+        },
+        id: 'hideLogoWrapperBackground',
+      });
+
+      tl
+        .set(refLogokWrapper.current, {
+          backgroundImage: 'none', left: '50%', width: '100%', x: '-50%',
+        });
+    },
+    [refLogokWrapper],
+  );
+
+  const showLogoWrapperBackground = useCallback(
+    () => {
+      const tl = gsap.timeline({
+        defaults: {
+          duration: '0.4',
+        },
+        id: 'showLogoWrapperBackground',
+      });
+
+      tl
+        .set(refLogokWrapper.current, { clearProps: 'backgroundImage' })
+        .fromTo(refLogokWrapper.current, { width: '100%' }, { left: '0', width: refLogo.current?.clientWidth, x: '0' }, '+=0.5');
+    },
+    [refLogokWrapper],
+  );
+
   useEffect(() => {
     showLogo();
+    hideLogoWrapperBackground();
+    showLogoWrapperBackground();
   }, []);
 
   return (
-    <div className={styles.ufcLogoWrapper}>
+    <div className={styles.ufcLogoWrapper} ref={refLogokWrapper}>
       <UFCLogo className={styles.ufcLogo} ref={refLogo} />
     </div>
   );
