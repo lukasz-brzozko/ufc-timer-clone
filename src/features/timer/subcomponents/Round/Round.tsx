0c6c41a2fb052d1d284b86bd4e580ed4259cbc39
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-18 17:17:11 +0200

    refac: use redux store for generating rounds

--- src/features/timer/subcomponents/Round/Round.tsx
+++ src/features/timer/subcomponents/Round/Round.tsx
@@ -1,17 +1,27 @@
+import { selectActiveRound, selectRoundsCounter } from '../../../roundCounter/roundCounterSlice';
+import { useAppSelector } from '../../../../app/hooks';
+
 import styles from './Round.module.scss';
 
-function Round() {
+function Round(): JSX.Element {
+  const { rounds } = useAppSelector(selectRoundsCounter);
+  const activeRound = useAppSelector(selectActiveRound);
+  const roundsArr: JSX.Element[] = [];
+
+  for (let i = 1; i < rounds + 1; i += 1) {
+    const isRoundCompletedModifier = i < activeRound ? ` ${styles.roundComplete}` : '';
+    const roundModifier = activeRound === i ? ` ${styles.roundActive}` : isRoundCompletedModifier;
+    const roundJSX = (
+      <div className={styles.roundWrapper} key={i}>
+        <span className={`${styles.round}${roundModifier}`} />
+      </div>
+    );
+    roundsArr.push(roundJSX);
+  }
+
   return (
     <div className={styles.roundContainer}>
-      <div className={styles.roundWrapper}>
-        <span className={`${styles.round} ${styles.roundComplete}`} />
-      </div>
-      <div className={styles.roundWrapper}>
-        <span className={`${styles.round} ${styles.roundActive}`} />
-      </div>
-      <div className={styles.roundWrapper}>
-        <span className={`${styles.round}`} />
-      </div>
+      {roundsArr}
     </div>
   );
 }
