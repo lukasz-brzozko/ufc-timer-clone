fa3f67be2103f0089f5af9e7a2991ecb53bfd4ad
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-24 21:24:41 +0200

    refac: create color constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,39 @@
+interface Color {
+  code: string
+  name: string,
+}
+export type Colors = Color[];
+
+const COLORS: Colors = [
+  {
+    code: '#131416',
+    name: 'Black',
+  },
+  {
+    code: '#294790',
+    name: 'Blue',
+  },
+  {
+    code: '#b5a772',
+    name: 'Gold',
+  },
+  {
+    code: '#207744',
+    name: 'Green',
+  },
+  {
+    code: '#8f020e',
+    name: 'Red',
+  },
+  {
+    code: '#f8f6f9',
+    name: 'White',
+  },
+  {
+    code: '#e6d450',
+    name: 'Yellow',
+  },
+
+];
+
+export default COLORS;
