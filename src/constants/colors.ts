ac6fb7bbf1206eb17f16bf689b3a8f972104b9d5
Lukasz Brzozko <lukasz9099@gmail.com>
2021-05-28 14:58:10 +0200

    refac: transform colors array into an object

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,39 +1,55 @@
-interface Color {
-  code: string
-  name: string,
+export interface Color {
+  code: string;
+  name: string;
+  textColor: string;
 }
-export type Colors = Color[];
 
-const COLORS: Colors = [
-  {
+export interface Colors {
+  [color: string]: Color
+}
+
+const COLORS: Colors = {
+  BLACK: {
     code: '#131416',
     name: 'Black',
-  },
-  {
-    code: '#294790',
-    name: 'Blue',
-  },
-  {
-    code: '#b5a772',
-    name: 'Gold',
-  },
-  {
-    code: '#207744',
-    name: 'Green',
-  },
-  {
-    code: '#8f020e',
-    name: 'Red',
-  },
-  {
-    code: '#f8f6f9',
-    name: 'White',
-  },
-  {
-    code: '#e6d450',
-    name: 'Yellow',
+    textColor: '#fff',
   },
 
-];
+  BLUE: {
+    code: '#294790',
+    name: 'Blue',
+    textColor: '#fff',
+  },
+
+  GOLD: {
+    code: '#b5a772',
+    name: 'Gold',
+    textColor: '#000',
+  },
+
+  GREEN: {
+    code: '#207744',
+    name: 'Green',
+    textColor: '#fff',
+  },
+
+  RED: {
+    code: '#8f020e',
+    name: 'Red',
+    textColor: '#fff',
+  },
+
+  WHITE: {
+    code: '#f8f6f9',
+    name: 'White',
+    textColor: '#000',
+  },
+
+  YELLOW: {
+    code: '#e6d450',
+    name: 'Yellow',
+    textColor: '#000',
+  },
+} as const;
 
 export default COLORS;
